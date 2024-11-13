52d51cafb4773d498273f975e130b8ec54e52dd9
Thiwanka1012 <160256984+Thiwanka1012@users.noreply.github.com>
2024-11-14 00:42:41 +0530

    I create main code

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,13 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        System.out.println("Hello\tworld");
+        System.out.println("Hello\nWorld");
+        System.out.println("Hello World\b");
+
+
+        System.out.println("\\");
+        System.out.println("\\\\");
     }
 
 }
