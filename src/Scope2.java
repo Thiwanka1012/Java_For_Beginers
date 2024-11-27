f76b6d52be2dfd487872506e832ea4115b5c1a67
Thiwanka1012 <160256984+Thiwanka1012@users.noreply.github.com>
2024-11-28 02:20:45 +0530

    I practiced Scope 2 with if else parts

---
+++ src/Scope2.java
@@ -0,0 +1,14 @@
+public class Scope2 {
+    public static void main(String[] args) {
+         int x;
+         int age=20;
+
+         if(age>=20){
+             x=100;
+         }
+         else{
+             x=30;
+         }
+        System.out.println(x);//
+    }
+}
