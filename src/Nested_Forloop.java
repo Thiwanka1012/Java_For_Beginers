7384a28dbe6c9b723be1c031f9311be65bef9aaf
Thiwanka1012 <160256984+Thiwanka1012@users.noreply.github.com>
2024-11-28 03:53:30 +0530

    Nested Forloop Part 1

---
+++ src/Nested_Forloop.java
@@ -0,0 +1,10 @@
+public class Nested_Forloop {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {  //1st Loop
+            for (int j = 0; j < 4; j++) {  //2 Loop
+                System.out.println("I:"+i + "|J:"+j);
+            }
+
+        }
+    }
+}
