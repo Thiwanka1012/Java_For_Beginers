2ea7e70d341136bc929073d35ffbcdf37295e577
Thiwanka1012 <160256984+Thiwanka1012@users.noreply.github.com>
2024-11-27 15:28:14 +0530

    Java

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,31 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
-        System.out.println("Hello\tworld");
-        System.out.println("Hello\nWorld");
-        System.out.println("Hello World\b");
 
+    Scanner input=new Scanner(System.in);
+        System.out.print("Input your number:");
+        int number= input.nextInt();
 
-        System.out.println("\\");
-        System.out.println("\\\\");
+        switch (number){
+            case 1:
+                System.out.println("1");
+                break;
+            case 2:
+                System.out.println("2");
+                break;
+            case 3:
+                System.out.println("3");
+                break;
+            case 4:
+                System.out.println("4");
+                break;
+            case 5:
+                System.out.println("5");
+                break;
+            default:
+                System.out.println("Wrong number");
+        }
     }
-
 }
+
