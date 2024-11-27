680135a8c80202dc1149e2e0783e953d99a81373
Thiwanka1012 <160256984+Thiwanka1012@users.noreply.github.com>
2024-11-28 02:34:26 +0530

    For Loop without Scoop

---
+++ src/ForLoop2.java
@@ -0,0 +1,10 @@
+public class ForLoop2 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++)
+            System.out.println("A"); //Print 5 times
+            System.out.println("B"); //Print 1 time
+
+
+
+    }
+}
