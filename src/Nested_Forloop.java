4fc70307a7daa2f8da4c26886bf4809f340e3ceb
Thiwanka1012 <160256984+Thiwanka1012@users.noreply.github.com>
2024-11-30 06:56:18 +0530

    Use 2sec Threading

--- src/Nested_Forloop.java
+++ src/Nested_Forloop.java
@@ -1,8 +1,9 @@
 public class Nested_Forloop {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         for (int i = 0; i < 5; i++) {  //1st Loop
             for (int j = 0; j < 4; j++) {  //2 Loop
                 System.out.println("I:"+i + "|J:"+j);
+                Thread.sleep(2000);
             }
 
         }
