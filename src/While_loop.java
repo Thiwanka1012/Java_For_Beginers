77bb21edadc4fb83aa5c71a00c10597b013866f8
Thiwanka1012 <160256984+Thiwanka1012@users.noreply.github.com>
2024-11-30 07:21:38 +0530

    Labaliced For loops.We can stop for loops as the lable.

---
+++ src/While_loop.java
@@ -0,0 +1,11 @@
+public class While_loop{
+    public static void main(String args[]){
+
+        int x=0;
+
+        while(x<10){
+            System.out.println("X:"+x);
+            x++;
+        }
+    }
+}
