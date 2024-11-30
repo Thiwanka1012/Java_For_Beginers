8714b8ebd4b99f8117ee8bf1e93495dbbc1a34a9
Thiwanka1012 <160256984+Thiwanka1012@users.noreply.github.com>
2024-11-30 08:21:49 +0530

    Return Keyword With methods calling.

---
+++ src/Return_keyword_with_methods.java
@@ -0,0 +1,23 @@
+public class Return_keyword_with_methods {
+    public static void main(String[] args) {
+
+        System.out.println("Main Method Start");                 //Starting main method
+        print();                                                //method calling
+        System.out.println("Main Method End");                //Ending main method
+
+    }
+
+    public static void print(){                                  //I call method named print
+
+        System.out.println("Print Method Start");              //Print method starting
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println("I:"+i);
+            if (i==5){
+                return;
+            }
+
+        }
+        System.out.println("Print Method Ending");             //Print method Ending
+    }
+}
