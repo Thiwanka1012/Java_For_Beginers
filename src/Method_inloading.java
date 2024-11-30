32bb80083926263103c20de1a4897b5b77cf1dee
Thiwanka1012 <160256984+Thiwanka1012@users.noreply.github.com>
2024-11-30 13:38:40 +0530

    Method calling(inloading)

---
+++ src/Method_inloading.java
@@ -0,0 +1,13 @@
+public class Method_inloading {
+    public static void main(String[] args) {      //Main method
+            printData();
+
+    }
+
+    public static void printData(){               //Sub method
+
+        System.out.println("Kamal");
+        System.out.println("Amal");
+        System.out.println("Nimal");
+    }
+}
