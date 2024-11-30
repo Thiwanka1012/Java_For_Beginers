1d4bd6c73be330ef82a1520751277b3c9cc1699b
Thiwanka1012 <160256984+Thiwanka1012@users.noreply.github.com>
2024-11-30 12:46:32 +0530

    Find digit count with while loop.

---
+++ src/Task1_Find_Digits.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Task1_Find_Digits {
+    public static void main(String[] args) {
+
+        Scanner input=new Scanner(System.in);
+        System.out.print("Insert A Number:");
+        int number= input.nextInt();
+
+        int digits=0;
+
+        while(number!=0){
+            number=number/10;                  //Floting point Division
+            digits++;
+        }
+        System.out.println("Digit Count is-"+digits);
+    }
+}
