e38185c0bdca12c8d9e28eca95443cf1071153ab
Thiwanka1012 <160256984+Thiwanka1012@users.noreply.github.com>
2024-11-30 07:32:18 +0530

    Do while loop

---
+++ src/Do_whileloop.java
@@ -0,0 +1,10 @@
+public class Do_whileloop {
+    public static void main(String args[]){
+
+        int x=0;
+        do {
+            System.out.println("x:"+x);
+            x++;
+        }while(x<10);
+    }
+}
