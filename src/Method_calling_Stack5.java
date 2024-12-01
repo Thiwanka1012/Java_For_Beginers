3d9ce1092785a92fb3c4d464ee2b55a324761ba8
Thiwanka1012 <160256984+Thiwanka1012@users.noreply.github.com>
2024-12-01 09:12:17 +0530

    Same parameeter and different arguments(Var arcs)

---
+++ src/Method_calling_Stack5.java
@@ -0,0 +1,16 @@
+public class Method_calling_Stack5 {
+    public static void main(String[] args) {
+
+        print(10);
+        print(10,20);
+        print(10,20,30);
+        print(10,20,30,40);
+
+
+
+
+    }
+    public static void print(int ...x){
+
+    }
+}
