a24139a78dbc0c9dc399595683c7440d93b3bdad
Thiwanka1012 <160256984+Thiwanka1012@users.noreply.github.com>
2024-12-01 08:25:22 +0530

    Method Callin Stack with initialized same variable in different methods.

---
+++ src/Method_calling_Stack_3.java
@@ -0,0 +1,15 @@
+public class Method_calling_Stack_3 {
+    public static void main(String[] args) {
+
+        int x=100;
+        System.out.println("Start main method");
+        print();
+        System.out.println("End main method");
+    }
+
+    public static void print(){
+        int x=100;
+        x++;
+        System.out.println(x);
+    }
+}
